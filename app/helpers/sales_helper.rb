ddd9e916b0f0e76bc18446893605d84d60f9d539
jeff-sexton <sexton.jeffp@gmail.com>
2020-04-29 16:52:19 -0700

    Updated sale helper to return largest sale object

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,6 +1,6 @@
 module SalesHelper
-  def active_sale?
-    Sale.active.order(:percent_off).any?
+  def active_sale
+    Sale.active.order(percent_off: :desc).first
     # return highest discount sale?
   end
 end
