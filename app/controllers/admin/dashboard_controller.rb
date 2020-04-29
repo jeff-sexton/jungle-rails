8ebb6b01cd202dd3dd4a2212636fc2af5ebd1fe6
jeff-sexton <sexton.jeffp@gmail.com>
2020-04-28 20:06:43 -0700

    added instance variables for admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   pass = ENV['ADMIN_PASS']
   http_basic_authenticate_with name: user, password: pass
   def show
+    @product_count = Product.count()
+    @category_count = Category.count()
+    @order_count = Order.count()
+    @order_revenue = Order.sum(:total_cents)
   end
 end
