03ed3d444cd56e720bda8789ff8edbf3c6f1a914
jeff-sexton <sexton.jeffp@gmail.com>
2020-05-05 15:32:35 -0700

    Completed user login feature test

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor logs in", type: :feature, js: true do
+
+  # Setup
+  before :each do
+    User.create!(
+      first_name: 'Francis',
+      last_name: 'Bacon',
+      email: 'baconTheDog@mail.com',
+      password: 'thisIsAPassword',
+      password_confirmation: 'thisIsAPassword'
+    )
+  end
+
+  scenario "They are redirected to the home page and their email is shown in the nav bar" do
+    # ACT
+    visit '/login'
+
+    fill_in "email", with: 'baconTheDog@mail.com'
+    fill_in 'password', with: 'thisIsAPassword'
+
+    click_on "Sign in"
+
+    # DEBUG / VERIFY
+    save_screenshot
+    # puts page.html
+
+    expect(page).to have_css 'section.products-index'
+  
+    within('nav') {
+      expect(page).to have_content('baconTheDog@mail.com')
+    }
+  end
+
+end
