7ef436d9bba17682df43cf72d0271b33d406c4c4
jeff-sexton <sexton.jeffp@gmail.com>
2020-05-05 16:31:59 -0700

    Fixed order show page to only allow access to correct user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,25 @@
 class OrdersController < ApplicationController
-
-  # before_filter :authorize
+  before_filter :authorize
 
   def index
     @orders = Order.where(email: current_user.email)
-
   end
 
   def show
-    @order = Order.find(params[:id])
-    @line_items = @order.line_items.map {|item| { line_item:item, product:Product.find(item.product_id) } }
+    @order =
+      Order.where('id = ? AND email = ?', params[:id], current_user.email).first
+    @order &&
+      @line_items =
+        @order.line_items.map do |item|
+          { line_item: item, product: Product.find(item.product_id) }
+        end
+
+    redirect_to :orders if !@order
   end
 
   def create
     charge = perform_stripe_charge
-    order  = create_order(charge)
+    order = create_order(charge)
 
     if order.valid?
       empty_cart!
@@ -22,7 +27,6 @@ class OrdersController < ApplicationController
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
-
   rescue Stripe::CardError => e
     redirect_to cart_path, flash: { error: e.message }
   end
@@ -30,28 +34,23 @@ class OrdersController < ApplicationController
   private
 
   def empty_cart!
-    # empty hash means no products in cart :)
     update_cart({})
   end
 
   def perform_stripe_charge
     Stripe::Charge.create(
-      source:      params[:stripeToken],
-      amount:      cart_subtotal_cents,
+      source: params[:stripeToken],
+      amount: cart_subtotal_cents,
       description: "Khurram Virani's Jungle Order",
-      currency:    'cad'
+      currency: 'cad'
     )
   end
 
   def create_order(stripe_charge)
     active_sale = Sale.highest_active
-    discount = ( active_sale && 1 - active_sale.percent_off / 100.00 ) || 1
+    discount = (active_sale && 1 - active_sale.percent_off / 100.00) || 1
 
-    order = Order.new(
-      email: params[:stripeEmail],
-      total_cents: cart_subtotal_cents,
-      stripe_charge_id: stripe_charge.id, # returned by stripe
-    )
+    order = Order.new()
 
     enhanced_cart.each do |entry|
       product = entry[:product]
@@ -67,6 +66,5 @@ class OrdersController < ApplicationController
     end
     order.save!
     order
-  end
-
+  end # empty hash means no products in cart :)
 end
