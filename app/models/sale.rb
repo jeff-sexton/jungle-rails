c5af03fe42d3105d0e9c8e35f7e98d539ae47afa
jeff-sexton <sexton.jeffp@gmail.com>
2020-05-05 19:17:39 -0700

    Added sale validations

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,8 @@
 class Sale < ActiveRecord::Base
+  validates :name, presence: true
+  validates :percent_off, { presence: true, numericality: true }
+  validate :valid_sale_dates?
+
   def self.highest_active
     where(
       'sales.starts_on <= ? AND sales.ends_on > ?',
@@ -25,5 +29,15 @@ class Sale < ActiveRecord::Base
 
   def active?
     !upcoming? && !finished?
+  end
+
+  private
+
+  def valid_sale_dates?
+    errors.add(:starts_on, 'must be a valid date') if !starts_on.is_a?(Date)
+    errors.add(:ends_on, 'must be a valid date') if !ends_on.is_a?(Date)
+    if ends_on && starts_on && ends_on < starts_on
+      errors.add(:ends_on, 'must be after sale start date')
+    end
   end
 end
